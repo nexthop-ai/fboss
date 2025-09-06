ec7781a590fcbdf0566a9804c1ebbec5585d8843
travisb-nexthop <travisb@nexthop.ai>
2025-09-05 17:52:46 -0700

    NO-ISS Build FBOSS against SAI 11.7.0 in manual builds (#47)
    #### Why I did it
    
    The SAI 11.7.0 GA is the best version we have at the moment. This change
    got lost somehow, so I'm committing it now.
    
    #### How I did it
    
    #### How to verify it
    
    #### Which release branch to backport
    
    - [ ] 202411

--- fboss/oss/scripts/nhfboss-build.sh
+++ fboss/oss/scripts/nhfboss-build.sh
@@ -8,8 +8,8 @@ common_options='--allow-system-packages --scratch-path /var/FBOSS/tmp_bld_dir --
 #export BUILD_SAI_FAKE=1
 #export BUILD_SAI_FAKE_LINK_TEST=1
 export SAI_BRCM_IMPL=1
-export SAI_SDK_VERSION=SAI_VERSION_12_2_0_0_ODP
-export SAI_VERSION=1.15.0
+export SAI_SDK_VERSION=SAI_VERSION_11_7_0_0_ODP
+export SAI_VERSION=1.14.0
 ./build/fbcode_builder/getdeps.py build --build-type MinSizeRel --no-deps $common_options
 
 popd >/dev/null
