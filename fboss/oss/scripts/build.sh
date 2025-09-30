965f5cb31d8b4e14276d2e555a130de265f8b27a
travisb-nexthop <travisb@nexthop.ai>
2025-09-30 09:21:57 -0700

    ISS-3193 Support FBOSS incremental builds against multiple SAIs (#62)
    #### Why I did it
    
    The binary outputs when building against different SAIs are not
    compatible and cause issues when switching from one SAI to another.
    Further, we want to support incrementally building against multiple
    SAIs so we don't want to overwrite intermediate build products
    anyways.
    
    Here we share downloads and extracted files across all builds to avoid
    that redundant work. However the rest of the build products are put into
    a SAI-version specific build directory.
    
    #### How I did it
    
    #### How to verify it
    
    #### Which release branch to backport
    
    - [ ] 202411

--- fboss/oss/scripts/build.sh
+++ fboss/oss/scripts/build.sh
@@ -4,15 +4,40 @@ set -e
 export SCCACHE_DIR=/var/src/.sccache
 export SCCACHE_CACHE_SIZE=30G
 
-common_options='--allow-system-packages --scratch-path /var/src/.build_dir --src-dir . --extra-cmake-defines {"CMAKE_C_COMPILER_LAUNCHER":"sccache","CMAKE_CXX_COMPILER_LAUNCHER":"sccache"} fboss'
-
-./build/fbcode_builder/getdeps.py install-system-deps --recursive fboss
-
 SAI_DIR=/var/src/.build_dir/sai
 source $SAI_DIR/sai_build.env
 
+if [ -n "$BUILD_SAI_FAKE" ]; then
+    sai_name="sai-fake"
+else
+    if [ -n "$SAI_BRCM_IMPL" ]; then
+        sai_name="sai-bcm-$SAI_SDK_VERSION"
+    else
+        sai_name="sai-unknown"
+    fi
+fi
+
+build_dir="/var/src/.build_dir/${sai_name}"
+mkdir -p $build_dir
+
+common_options="--allow-system-packages --scratch-path ${build_dir} --src-dir . --extra-cmake-defines {\"CMAKE_C_COMPILER_LAUNCHER\":\"sccache\",\"CMAKE_CXX_COMPILER_LAUNCHER\":\"sccache\"} fboss"
+
+# Share download caches
+mkdir -p /var/src/.build_dir/downloads
+[ ! -L $build_dir/downloads ] && ln -s /var/src/.build_dir/downloads $build_dir/downloads
+mkdir -p /var/src/.build_dir/repos
+[ ! -L $build_dir/repos ] && ln -s /var/src/.build_dir/repos $build_dir/repos
+mkdir -p /var/src/.build_dir/extracted
+[ ! -L $build_dir/extracted ] && ln -s /var/src/.build_dir/extracted $build_dir/extracted
+
+# The build will modify these files when building against a real SAI. Snapshot these files so they can be restored. We
+# shouldn't use something like git stash because we are running as root inside the build container.
+tar -cf manifests_snapshot.tar build/fbcode_builder/manifests/fboss build/fbcode_builder/manifests/libsai
+
+./build/fbcode_builder/getdeps.py install-system-deps --recursive fboss
+
 if [ -z "$BUILD_SAI_FAKE" ]; then
-    ./fboss/oss/scripts/build-helper.py $SAI_DIR/lib/libsai_impl.a $SAI_DIR/include /var/src/.build_dir/sai_impl $SAI_VERSION
+    ./fboss/oss/scripts/build-helper.py $SAI_DIR/lib/libsai_impl.a $SAI_DIR/include $build_dir/sai_impl $SAI_VERSION
 fi
 
 echo "Building deps"
@@ -21,5 +46,9 @@ echo "Building deps"
 echo "Building FBOSS"
 ./build/fbcode_builder/getdeps.py build --build-type MinSizeRel --no-deps $common_options
 
-./fboss/oss/scripts/package-fboss.py --scratch-path /var/src/.build_dir/ --compress
-mv /var/src/.build_dir/fboss_bins.tar.zst .
+./fboss/oss/scripts/package-fboss.py --scratch-path $build_dir/ --compress
+mv $build_dir/fboss_bins.tar.zst .
+
+# Restore modified manifests
+tar -xf manifests_snapshot.tar
+rm manifests_snapshot.tar
