f8caa314cdce7074b81cb2831b551969e63e1fdf
Travis Brown <travisb@nexthop.ai>
2025-12-22 14:03:12 -0800

    NOS-359 More resilient argument parsing for Distro Infra (#214)
    <!-- Thanks for submitting a pull request! We appreciate you spending
    the time to work on these changes. Please provide enough information so
    that others can review your pull request. -->
    
    **Pre-submission checklist**
    - [x] I've ran the linters locally and fixed lint errors related to the
    files I modified in this PR. You can install the linters by running `pip
    install -r requirements-dev.txt && pre-commit install`
    - [x] `pre-commit run`
    
    # Summary
    
    <!-- Explain the motivation for making this change and any other context
    that you think would help reviewers of your code. What existing problem
    does the pull request solve? -->
    
    Address the review request for more resilient argument parsing. Now all
    options have option names and are no longer positional.
    
    # Test Plan
    
    <!-- Demonstrate the code is solid. Example: The exact commands you ran
    and their output, screenshots / videos if the pull request changes the
    user interface. How exactly did you verify that your PR solves the issue
    you wanted to solve? -->
    
    <!-- If a relevant Github issue exists for this PR, please make sure you
    link that issue to this PR -->

--- fboss-image/distro_infra/parts/run_distro_infra.sh
+++ fboss-image/distro_infra/parts/run_distro_infra.sh
@@ -1,14 +1,57 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <interface>"
+INTERFACE=""
+
+help() {
+    echo "Usage: $0 --intf <interface>"
+    echo ""
+    echo "Run FBOSS Distro Infrastructure PXE boot services"
+    echo ""
+    echo "Options:"
+    echo ""
+    echo "  -i|--intf <interface>       Network interface to attach to (must have L2 adjacency with FBOSS duts)"
+    echo ""
+    echo "  -h|--help                   Print this help message"
+    echo ""
+    echo "Examples:"
+    echo "  $0 --intf vlan1033"
+    echo "  $0 -i eth0"
+    echo ""
+}
+
+if [[ $# -eq 0 ]]; then
+    echo "Error: No arguments provided"
+    help
+    exit 1
+else
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            -i|--intf)
+                INTERFACE="$2"
+                shift 2
+                ;;
+            -h|--help)
+                help
+                exit 0
+                ;;
+            *)
+                echo "Error: Unrecognized command option: '${1}'"
+                help
+                exit 1
+                ;;
+        esac
+    done
+fi
+
+if [[ -z "$INTERFACE" ]]; then
+    echo "Error: --intf is required"
+    help
     exit 1
 fi
 
-intf=$1
-v4_ip=$(ip -4 addr show dev $intf | awk -F '[[:space:]/]+' '/inet/{print $3}')
-v6_ip=$(ip -6 addr show dev $intf scope global | awk -F '[[:space:]/]+' '/inet6/{print $3; exit}')
-echo "Listening on ${intf} - ${v4_ip} & ${v6_ip}"
+v4_ip=$(ip -4 addr show dev $INTERFACE | awk -F '[[:space:]/]+' '/inet/{print $3}')
+v6_ip=$(ip -6 addr show dev $INTERFACE scope global | awk -F '[[:space:]/]+' '/inet6/{print $3; exit}')
+echo "Listening on ${INTERFACE} - ${v4_ip} & ${v6_ip}"
 
 mkdir -p -m 777 /distro_infra/persistent/cache
 cp /distro_infra/ipxev4.efi /distro_infra/persistent/cache
@@ -22,7 +65,7 @@ nginx -c /distro_infra/nginx.conf  -p /distro_infra/persistent
 # Minimize responding to other devices
 echo 'tag:!fbossdut,ignore' > /distro_infra/dnsmasq_conf.d/default_ignore
 
-dnsmasq --interface=${intf} --no-daemon \
+dnsmasq --interface=${INTERFACE} --no-daemon \
     --log-debug --log-dhcp \
     --port=0 \
     --enable-tftp \
@@ -34,7 +77,7 @@ dnsmasq --interface=${intf} --no-daemon \
     --dhcp-range=tag:fbossdut,${v4_ip},proxy \
     --pxe-service=tag:fbossdut,x86-64_EFI,ipxe,ipxev4.efi \
     --enable-ra \
-    --dhcp-range=tag:fbossdut,::fb05:5000:0001,::fb05:50ff:ffff,constructor:$intf,5m \
+    --dhcp-range=tag:fbossdut,::fb05:5000:0001,::fb05:50ff:ffff,constructor:$INTERFACE,5m \
     --dhcp-option=tag:fbossdut,option6:bootfile-url,tftp://[${v6_ip}]/ipxev6.efi &
 
 sleep 2 # Wait for dnsmasq log spew
