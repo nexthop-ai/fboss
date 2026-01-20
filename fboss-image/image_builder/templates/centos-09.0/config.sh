52a3680f61b7b753edd030bb456cd89062bbcc21
Travis Brown <travisb@nexthop.ai>
2026-01-20 13:17:13 -0800

    NOS-3584 Create device-local RPM repo for run-time installation (#319)
    <!-- Thanks for submitting a pull request! We appreciate you spending
    the time to work on these changes. Please provide enough information so
    that others can review your pull request. -->
    
    **Pre-submission checklist**
    - [ ] I've ran the linters locally and fixed lint errors related to the
    files I modified in this PR. You can install the linters by running `pip
    install -r requirements-dev.txt && pre-commit install`
    - [ ] `pre-commit run`
    
    # Summary
    
    <!-- Explain the motivation for making this change and any other context
    that you think would help reviewers of your code. What existing problem
    does the pull request solve? -->
    
    package_manager, by default, detects the platform it is running on then
    uses a compiled-in config file to install the platform BSP RPM using
    dnf.
    
    This requires that we have an RPM repo with those RPMs. In Distro Image
    we cannot presume an infrastructure RPM server with the correct BSP and
    dependent RPMs. Instead, create a device-local one for that purpose.
    
    The repo metadata is created at boot instead of install time to help the
    development workflows where a new RPM is uploaded to the device, then
    the device is restarted for testing.
    
    # Test Plan
    
    <!-- Demonstrate the code is solid. Example: The exact commands you ran
    and their output, screenshots / videos if the pull request changes the
    user interface. How exactly did you verify that your PR solves the issue
    you wanted to solve? -->
    
    <!-- If a relevant Github issue exists for this PR, please make sure you
    link that issue to this PR -->
    
    Build and load the image on a dut. Manually copy the BSP RPM into
    /usr/local/share/local_rpm_repo, run createrepo
    /usr/local/share/local_rpm_repo, then dnf search for that RPM:
    ```
    dnf search nexthop
    Repository local_rpm_repo is listed more than once in the configuration
    Last metadata expiration check: 0:00:15 ago on Sat 17 Jan 2026 12:44:46 AM UTC.
    ======================= Name & Summary Matched: nexthop ========================
    nexthop_bsp_kmods-6.11.1-1.fboss.el9.x86_64-1.el9-1.0.0.x86_64 : Nexthop BSP Kernel Modules
    ```
    
    ---------
    
    Co-authored-by: marif-nexthop <marif@nexthop.ai>

--- fboss-image/image_builder/templates/centos-09.0/config.sh
+++ fboss-image/image_builder/templates/centos-09.0/config.sh
@@ -6,6 +6,10 @@ echo "--- Executing $0 ---"
 sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="FBOSS Distro Image"/' /usr/lib/os-release
 sed -i 's/^NAME=.*/NAME="FBOSS Distro Image"/' /usr/lib/os-release
 
+# All dnf invocations with an invalid RPM repo configured will fail. Create the
+# metadata for the local_rpm_repo now to prevent that.
+createrepo /usr/local/share/local_rpm_repo
+
 # 1. Process component artifacts and install RPMs
 #
 # Component artifacts are copied to /repos/<component_name>/
@@ -257,6 +261,7 @@ echo "Copied all GRUB modules to /boot/grub2/x86_64-efi/ (root partition)"
 
 # 7. Enable systemd services
 echo "Enabling FBOSS systemd services..."
+systemctl enable local_rpm_repo.service
 systemctl enable platform_manager.service
 systemctl enable data_corral_service.service
 systemctl enable fan_service.service
