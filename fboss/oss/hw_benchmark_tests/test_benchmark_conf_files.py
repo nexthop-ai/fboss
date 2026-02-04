a5adb47e1a544fc073020735c35cdf1801277baa
Anna Komarova <anna@nexthop.ai>
2026-02-04 11:33:04 -0800

    NO-NOS: remove the rando license statement slipped in by Auggie (#422)
    Auggie added in a comment about the BSD license, which is only used for
    c++ files in the Fboss codebase. Python files just state that it is
    copyrighted by Meta et al.

--- fboss/oss/hw_benchmark_tests/test_benchmark_conf_files.py
+++ fboss/oss/hw_benchmark_tests/test_benchmark_conf_files.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
 
 """
 Validates that T1, T2, and additional benchmark configuration files
