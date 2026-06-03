fae34967975cd11868666a363c23d405ea8acabf
Travis Brown <travisb@nexthop.ai>
2026-06-02 17:53:32 -0700

    NO-DEVX Always build dev container with clang (#1004)
    # Summary
    
    We should always be using clang these days. Eliminate one avenue of
    mistake by removing the option to build a gcc container.
    
    # Test Plan
    
    Manual testing.
    
    # Upstream required
    
    - [ ] Yes — ensure PR title has no sensitive info; it's used as the
    upstream branch + commit message unless overridden below. Jira IDs in
    title are ignored.
    
      - [ ] Custom commit message
        ```text
        <custom commit message>
        ```
      - [ ] Custom branch name
        ```text
        <custom-branch-name>
        ```
      - [ ] Cherry-pick now (runs immediately; later changes must be manual)
    
    - [X] No

--- fboss/oss/scripts/nhfboss-docker-build.py
+++ fboss/oss/scripts/nhfboss-docker-build.py
@@ -5,11 +5,7 @@
 # allowing the container to be reused for faster incremental development cycles.
 
 import importlib
-import os
+
 docker_build = importlib.import_module("docker-build")
 
-use_clang = os.getenv("USE_CLANG", "").lower() in ("true", "1", "yes")
-if use_clang:
-    print("Using clang for build (experimental)")
-
-docker_build.build_docker_image(docker_build.get_docker_path(), use_clang)
+docker_build.build_docker_image(docker_build.get_docker_path(), use_clang=True)
