04dc2abcc0b3dbd4b18550e630a83ed1428f52f8
maisey-nexthop <maisey@nexthop.ai>
2026-08-28 13:18:43 -0700

    [Nexthop][m4062nhp] Fix SIGABRT in HwFlowletStatsCollection benchmark on SAI switch
    Signed-off-by: maisey-nexthop <maisey@nexthop.ai>

--- fboss/agent/hw/sai/hw_test/HwTestFlowletUtilsThriftHandler.cpp
+++ fboss/agent/hw/sai/hw_test/HwTestFlowletUtilsThriftHandler.cpp
@@ -1,6 +1,5 @@
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
-#include "fboss/agent/FbossError.h"
 #include "fboss/agent/hw/sai/switch/SaiSwitch.h"
 #include "fboss/agent/hw/test/HwTestThriftHandler.h"
 #include "fboss/agent/state/RouteNextHopEntry.h"
@@ -8,7 +7,7 @@
 namespace facebook::fboss::utility {
 
 void HwTestThriftHandler::updateFlowletStats() {
-  throw FbossError("Flowlet stats are not supported in SaiSwitch.");
+  hwSwitch_->getHwFlowletStats();
 }
 
 cfg::SwitchingMode HwTestThriftHandler::getFwdSwitchingMode(
